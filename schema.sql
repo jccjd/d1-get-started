b1bdde68d123a90b9a08cc7e1a34807f99648adf
jccjd <jccjdli@gmail.com>
2025-12-11 14:14:53 +0800

    Replace Customers table with todos table

--- schema.sql
+++ schema.sql
@@ -1,3 +1,8 @@
-DROP TABLE IF EXISTS Customers;
-CREATE TABLE IF NOT EXISTS Customers (CustomerId INTEGER PRIMARY KEY, CompanyName TEXT, ContactName TEXT);
-INSERT INTO Customers (CustomerID, CompanyName, ContactName) VALUES (1, 'Alfreds Futterkiste', 'Maria Anders'), (4, 'Around the Horn', 'Thomas Hardy'), (11, 'Bs Beverages', 'Victoria Ashworth'), (13, 'Bs Beverages', 'Random Name');
+DROP TABLE IF EXISTS todos;
+CREATE TABLE IF NOT EXISTS todos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    completed INTEGER DEFAULT 0,
+    created_at INTEGER DEFAULT (strftime('%s', 'now'))
+);
+INSERT INTO todos (title) VALUES ('学习 Cloudflare Workers');
